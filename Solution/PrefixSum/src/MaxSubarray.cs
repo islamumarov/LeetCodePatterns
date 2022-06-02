d8be99b8179b11c04c3dcf8e12f17610ed2c0666
ripdotnet <Ihtyyarovich@gmail.com>
2022-06-02 18:45:49 +0300

    added new solution https://leetcode.com/problems/maximum-subarray

---
+++ Solution/PrefixSum/src/MaxSubarray.cs
@@ -0,0 +1,26 @@
+namespace src;
+
+public class MaxSubarray
+{
+
+    /// <summary>
+    /// Given an integer array nums, find the contiguous subarray (containing at least one number)
+    /// which has the largest sum and return its sum.
+    /// A subarray is a contiguous part of an array.
+    /// </summary>
+    /// <param name="nums">array nums</param>
+    /// <returns>the largest sum</returns>
+    public static int FindLargestSum(int[] nums)
+    {
+        if (nums.Length < 2) return nums[0];
+        int maxSF = int.MinValue;
+        int current = 0;
+        foreach (var i in nums)
+        {
+            current = Math.Max(current + i, i);
+            maxSF = Math.Max(current, maxSF);
+        }
+
+        return maxSF;
+    }
+}

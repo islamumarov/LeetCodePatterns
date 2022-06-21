5871072431395a769ca41812bf97aa57e39baf81
ripdotnet <Ihtyyarovich@gmail.com>
2022-06-22 00:59:00 +0300

    update shortest path pattern

---
+++ Solution/Dijkstra/src/PathMaxProbability.cs
@@ -0,0 +1,13 @@
+namespace src;
+
+public class PathMaxProbability
+{
+    public static double MaxProbability(int n, int[][] edges, double[] succProb, int start, int end)
+    {
+        double res = 0;
+        
+        PriorityQueue<int, double> path = new PriorityQueue<int, double>();
+
+        return res;
+    }
+}

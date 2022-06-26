ccbf3d0cf83837d8b6dd809b121754324113f3b8
ripdotnet <Ihtyyarovich@gmail.com>
2022-06-26 17:13:43 +0300

    added new pattern and first related task

---
+++ Solution/In-Place_Reversal_LinkedList/206.reverse-linked-list.cs
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=206 lang=csharp
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+
+// Definition for singly-linked list.
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        if (head == null) return head;
+
+        ListNode current = null;
+        ListNode prev = head;
+        ListNode next = null;
+
+        while (prev != null)
+        {
+            next = prev.next;
+            prev.next = current;
+            current = prev;
+            prev = next;
+        }
+        return current;
+    }
+}
+// @lc code=end
+

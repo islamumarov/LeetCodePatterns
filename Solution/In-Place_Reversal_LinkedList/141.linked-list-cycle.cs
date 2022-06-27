04630a9220a4b35dca7b882101e95bb7c9081dfc
ripdotnet <Ihtyyarovich@gmail.com>
2022-06-27 21:33:33 +0300

    classwork

---
+++ Solution/In-Place_Reversal_LinkedList/141.linked-list-cycle.cs
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=141 lang=csharp
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ using System.Collections.Generic;
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<ListNode>();
+        if (head == null) return false;
+        set.Add(head);
+        var node = head.next;
+        while (node != null)
+        {
+            if (set.Contains(node))
+            {
+                return true;
+            }
+            set.Add(node);
+            node = node.next;
+        }
+        return false;
+    }
+}
+// @lc code=end
+

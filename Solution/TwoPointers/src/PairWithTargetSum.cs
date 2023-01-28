dab1aba0e52894882d525c4882649462be4af11f
islamumarov <Ihtyyarovich@gmail.com>
2023-01-28 20:51:05 +0300

    added new solution to https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/

--- Solution/TwoPointers/src/PairWithTargetSum.cs
+++ Solution/TwoPointers/src/PairWithTargetSum.cs
@@ -4,6 +4,19 @@ public class PairWithTargetSum
 {
     public static int[] Search(int[] arr, int targetSum)
     {
-        
+        var (left, right) = (0, arr.Length);
+        while (left < right)
+        {
+            int sum = arr[left] + arr[right];
+            if(sum == targetSum)
+                return new[] { left, right };
+            if (sum > targetSum)
+                right--;
+            else
+                left++;
+
+        }
+
+        return new[] { -1, -1 };
     }
 }

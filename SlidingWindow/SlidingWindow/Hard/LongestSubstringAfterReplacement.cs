c31ec4eb67c7ac742ad93489ea7baa2d94970168
islamumarov <ihtyyarovich@gmail.com>
2023-09-19 00:58:45 +0300

    added new task Longest Substring with same letter After Replacements

---
+++ SlidingWindow/SlidingWindow/Hard/LongestSubstringAfterReplacement.cs
@@ -0,0 +1,19 @@
+namespace SlidingWindow.Hard;
+
+public static class LongestSubstringAfterReplacement
+{
+    /// <summary>
+    /// Given a string with lowercase letters only,
+    /// if you are allowed to replace no more than ‘k’ letters with any letter,
+    /// find the length of the longest substring having the same letters after replacement.
+    /// </summary>
+    /// <param name="str">Given string</param>
+    /// <param name="k">allowed replacement</param>
+    /// <returns>length of the longest substring</returns>
+    public static int FindLength(string str, int k)
+    {
+        var maxLen = -1;
+
+        return maxLen;
+    }
+}

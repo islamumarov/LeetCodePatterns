d1835fa92c19f0eca58a6207bc70c2bf97072f25
Islam Umarov <Ihtyyarovich@gmail.com>
2024-04-05 18:39:13 +0300

    added daily leetcode challenge: 05.04.2024

---
+++ DailyLeetCode.Test/MakeTheStringGreatTests.cs
@@ -0,0 +1,14 @@
+﻿namespace DailyLeetCode.Test;
+
+public class MakeTheStringGreatTests
+{
+    [Theory]
+    [InlineData("leEeetcode", "leetcode")]
+    [InlineData("abBAcC", "")]
+    [InlineData("s", "s")]
+    public void MakeTheStringGreat_ShouldReturnCorrectResult(string input, string expected)
+    {
+        var result = MakeTheStringGreat.MakeGood(input);
+        Assert.Equal(expected, result);
+    } 
+}

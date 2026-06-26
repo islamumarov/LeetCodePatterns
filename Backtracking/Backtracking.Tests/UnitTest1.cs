255fa2d50d60f3a266297b7dcda270c67cf7bc13
islam <Ihtyyarovich@gmail.com>
2026-06-26 11:51:37 +0300

    Backtracking

---
+++ Backtracking/Backtracking.Tests/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿namespace Backtracking.Tests;
+
+public class SubsetsTests
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test1(int[] nums, IList<IList<int>> expected)
+    {
+        var actual = Subsets78.Subsets(nums);
+        Assert.Equal(expected, actual);
+    }
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return
+        [
+            new int[] { 1, 2, 3 },
+            new List<IList<int>>
+            {
+                new List<int> { },
+                new List<int> { 1 },
+                new List<int> { 2 },
+                new List<int> { 3 },
+                new List<int> { 1, 2 },
+                new List<int> { 1, 3 },
+                new List<int> { 2, 3 },
+                new List<int> { 1, 2, 3 }
+            }
+        ];
+
+        yield return new object[]
+        {
+            new int[] { 0 },
+            new List<IList<int>>
+            {
+                new List<int> { },
+                new List<int> { 0 }
+            }
+        };
+    }
+}
---
+++ Backtracking/Backtracking/Subsets78.cs
@@ -0,0 +1,26 @@
+﻿namespace Backtracking;
+
+public class Subsets78
+{
+    public static IList<IList<int>> Subsets(int[] nums)
+    {
+        var result = new List<IList<int>>();
+        var curPath = new Stack<int>();
+        void Backtrack(int start)
+        {
+            result.Add(curPath.ToList());
+
+            for (int i = start; i < nums.Length; i++)
+            {
+                curPath.Push(nums[i]);
+                Backtrack(i + 1);
+                curPath.Pop();
+            }
+        }
+
+        Backtrack(0);
+
+        return result;
+    }
+}
+
